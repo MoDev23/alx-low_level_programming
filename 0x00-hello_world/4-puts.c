17bd29d2375a4e4e40a92bea5547a71c00caf16d
Moses Ola <oladejimoses0@gmail.com>
2023-02-16 14:29:30 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
 * Return: 0 (Success)
 */
 int main(void)
-{puts("\"Programming is like building a multiligual puzzle");
+{
+puts("\"Programming is like building a multiligual puzzle");
 return (0);
 }
